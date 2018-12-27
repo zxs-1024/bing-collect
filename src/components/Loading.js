904052e9d2eba366494dbfe73ad9d65c8ac150dc
hao.zhang <735296233@qq.com>
2018-12-27 23:33:21 +0800

    :recycle: Loading components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import '../style/loading.scss';
 
-function Loading() {
+const icon = {
+  DuaRing: <DuaRing />,
+  Ripple: <Ripple />,
+  Ellipsis: <Ellipsis />,
+  Pacman: <Pacman />
+};
+
+function Loading({ type }) {
+  return <div>{icon[type]}</div>;
+}
+
+function DuaRing({ type }) {
+  return <div className="lds-dual-ring" />;
+}
+
+function Ripple({ type }) {
   return (
-    // <div className="lds-dual-ring" />
     <div className="lds-ripple">
       <div />
       <div />
     </div>
-    // <div className="lds-ellipsis">
-    //   <div />
-    //   <div />
-    //   <div />
-    //   <div />
-    // </div>
+  );
+}
+
+function Ellipsis({ type }) {
+  return (
+    <div className="lds-ellipsis">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
+
+function Pacman({ type }) {
+  return (
+    <div style={{ width: '100%', height: '100%' }} className="lds-pacman">
+      <div>
+        <div />
+        <div />
+        <div />
+      </div>
+      <div>
+        <div />
+        <div />
+      </div>
+    </div>
   );
 }
 
