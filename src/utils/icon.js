17e1f1c6eeb1a7a30e94f489ce3d4b3abb58a2dc
hao.zhang <735296233@qq.com>
2019-04-22 22:50:33 +0800

    :tada: Initial commit.

--- src/utils/icon.js
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faArrowAltCircleDown,
-  faHeart
-} from '@fortawesome/free-solid-svg-icons';
-
-library.add(faArrowAltCircleDown);
-library.add(faHeart);
