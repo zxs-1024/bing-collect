6a09d31e8c13badb3e2ec214b83c32d06bb69482
hao.zhang <735296233@qq.com>
2019-07-05 22:05:06 +0800

    :memo: use react-github-corner

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react'
+import GithubCorner from 'react-github-corner'
 
 import Header from '@components/Header/index'
 import Footer from '@components/Footer/index'
 
 import classes from './index.module.scss'
 
-// 
 const MainLayout: React.FC = props => {
+  const customHref = 'https://github.com/zhanghao-zhoushan/bing-app'
+
   return (
     <div className={classes.mainLayout}>
+      <GithubCorner
+          href={customHref}
+          bannerColor="#F5BB41"
+          octoColor="#fff"
+          size={80}
+          direction="right"
+          svgStyle={{ mixBlendMode: 'darken' }}
+        />
       <Header />
       <main className={`${classes.mainContainer}`}>{props.children}</main>
       <Footer />
