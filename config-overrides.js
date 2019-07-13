c0f968d3e34facafe0949e42198dded675332efd
hao.zhang <735296233@qq.com>
2019-07-13 19:13:21 +0800

    config: remove build sourceMap

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
-module.exports = function override(config, env) {
+process.env.GENERATE_SOURCEMAP = 'production' ? false : true
+
+module.exports = function override (config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
     '@': path.resolve(__dirname, 'src'),
